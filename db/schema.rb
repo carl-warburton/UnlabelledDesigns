e75c1e020271a71df16113bfa70ff8152e1e6f2f
carl-warburton <carl.warburton1992@gmail.com>
2017-07-23 10:32:23 +1000

    Updated Status for enum

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20170723000310) do
     t.text     "body"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.integer  "status",     default: 0
     t.string   "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
   end
 
